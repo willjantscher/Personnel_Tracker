ccdf1538abc8cd7481903fcb60f8e2dc86719642
ariagoose <73117176+ariagoose@users.noreply.github.com>
2020-12-15 17:21:52 -0500

    Initialized Single Spa

---
+++ Inbound_Outbound_Tracker/webpack.config.js
@@ -0,0 +1,15 @@
+const webpackMerge = require("webpack-merge");
+const singleSpaDefaults = require("webpack-config-single-spa-react");
+
+module.exports = (webpackConfigEnv, argv) => {
+  const defaultConfig = singleSpaDefaults({
+    orgName: "team6",
+    projectName: "personnelTracker",
+    webpackConfigEnv,
+    argv,
+  });
+
+  return webpackMerge.smart(defaultConfig, {
+    // modify the webpack config however you'd like to by adding to this object
+  });
+};
