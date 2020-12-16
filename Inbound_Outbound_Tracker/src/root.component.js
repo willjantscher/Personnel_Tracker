bac66817ca45cb61522502fa37f80bd8b4ffea39
ariagoose <73117176+ariagoose@users.noreply.github.com>
2020-12-16 12:59:17 -0500

    Working with Fake Data

--- Inbound_Outbound_Tracker/src/root.component.js
+++ Inbound_Outbound_Tracker/src/root.component.js
@@ -1,7 +1,7 @@
 import React from "react";
-import Test from "./PCS_Tracker.jsx"
+import PCS_Tracker from "./PCS_Tracker.jsx"
 
 export default function Root(props) {
-  return <PCS_Tracker />;
+  return <PCS_Tracker/>;
 }
 
