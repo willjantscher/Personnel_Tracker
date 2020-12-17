bd157cf518c995b58260b8d61120930ecace0f84
willjantscher <william.jantscher@gmail.com>
2020-12-17 08:51:10 -0700

    updated the db to include the paygrad

--- _Database/src/main/java/com/example/demo/Controller.java
+++ _Database/src/main/java/com/example/demo/Controller.java
@@ -1,5 +1,7 @@
 package com.example.demo;
 //./gradlew bootRun
+//docker-compose up
+//docker-compose down -v
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
