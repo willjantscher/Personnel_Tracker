0876228379483491d220b9e0be5e9d361799d31d
ariagoose <73117176+ariagoose@users.noreply.github.com>
2020-12-18 12:00:25 -0500

    data accurately divided into inbounds/outbounds and formatted into table
    Also removed test data

--- Inbound_Outbound_Tracker/src/components/ListMembers.jsx
+++ Inbound_Outbound_Tracker/src/components/ListMembers.jsx
@@ -3,14 +3,111 @@ import React from 'react'
 
 const ListMembers = (props) => {
     
-    let members = props.memberList.map(member => <li>{member.first_name}</li> );
-    console.log(members);
+    let inboundMembers = props.memberList.filter((inboundMember) => (inboundMember.arrival_date !== null));
+    let outboundMembers = props.memberList.filter((outboundMember) => (outboundMember.departure_date !== null));   
 
     return(
-        <ul>
-            {members}    
-        </ul>
+        <div>
+            <h4>Inbound Members</h4>
+            <div className="row">
+            <table className="table table-striped table-bordered table-hover">
+            <thead>
+              <tr>
+                <th scope="col">Paygrade</th>
+                <th scope="col">First Name</th>
+                <th scope="col">Last Name</th>
+                <th scope="col">RNLTD</th>
+              </tr>
+            </thead>
+            <tbody>
+            {inboundMembers.map((inbounds) => (
+                <tr>
+                  <td> {inbounds.paygrade} </td>
+                  <td> {inbounds.first_name} </td>
+                  <td> {inbounds.last_name} </td>
+                  <td> {inbounds.arrival_date}</td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        </div>
+        <div>
+          <h4>Outbounds</h4>
+        </div>
+        <div className="row">
+          <table className="table table-striped table-bordered table-hover">
+            <thead>
+              <tr>
+                <th scope="col">Paygrade</th>
+                <th scope="col">First Name</th>
+                <th scope="col">Last Name</th>
+                <th scope="col">Departure Date</th>
+              </tr>
+            </thead>
+            <tbody>
+              {outboundMembers.map((outbounds) => (
+                <tr>
+                  <td> {outbounds.paygrade} </td>
+                  <td> {outbounds.first_name} </td>
+                  <td> {outbounds.last_name} </td>
+                  <td> {outbounds.departure_date}</td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        </div>
+        </div>
     );
 }
 
 export default ListMembers
+
+/*</div><div>
+          <h4>Inbounds</h4>
+        <div className="row">
+          <table className="table table-striped table-bordered table-hover">
+            <thead>
+              <tr>
+                <th scope="col">Rank</th>
+                <th scope="col">First Name</th>
+                <th scope="col">Last Name</th>
+                <th scope="col">RNLTD</th>
+              </tr>
+            </thead>
+            <tbody>
+              {this.state.inbounds.map((inbounds) => (
+                <tr>
+                  <td> {inbounds.rank} </td>
+                  <td> {inbounds.firstName} </td>
+                  <td> {inbounds.lastName} </td>
+                  <td> {inbounds.RNLTD}</td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        </div>
+        <div>
+          <h4>Outbounds</h4>
+        </div>
+        <div className="row">
+          <table className="table table-striped table-bordered table-hover">
+            <thead>
+              <tr>
+                <th scope="col">Rank</th>
+                <th scope="col">First Name</th>
+                <th scope="col">Last Name</th>
+                <th scope="col">Departure Date</th>
+              </tr>
+            </thead>
+            <tbody>
+              {this.state.outbounds.map((outbounds) => (
+                <tr>
+                  <td> {outbounds.rank} </td>
+                  <td> {outbounds.firstName} </td>
+                  <td> {outbounds.lastName} </td>
+                  <td> {outbounds.departureDate}</td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+          */
